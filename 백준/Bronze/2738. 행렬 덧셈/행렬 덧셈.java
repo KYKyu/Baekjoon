de9f5d6fd23258dd997bc36992af48537f6ef322
KYKyu <130036932+KYKyu@users.noreply.github.com>
2023-10-13 17:34:47 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 632 ms, Memory: 35800 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int row, col;
+        row = sc.nextInt();
+        col = sc.nextInt();
+        
+        int[][] array = new int[row][col];
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                array[i][j] = sc.nextInt();
+            }
+        }
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                array[i][j] += sc.nextInt();
+            }
+        }
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+        sc.close();
+    }
+}
