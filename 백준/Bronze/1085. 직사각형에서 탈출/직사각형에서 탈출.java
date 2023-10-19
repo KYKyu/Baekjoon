196678b430165f39d18bf0f5ceed9a516bb8f464
KYKyu <130036932+KYKyu@users.noreply.github.com>
2023-10-19 11:01:02 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,26 +1,23 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
+
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int[] array = new int[4];
-		for(int i = 0; i < 4; i++) {
-			array[i] = Integer.parseInt(st.nextToken());
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+        
+		int[] arr = {x,w-x,y,h-y};
+		int min = x;
+		for(int i=0;i<4;i++) {
+			if(min > arr[i]) {
+				min = arr[i];
+			}
 		}
-
-		array[2] = array[2] - array[0];
-		array[3] = array[3] - array[1];
-		int min = array[0];
-		for(int i = 1; i < 4; i++) {
-			if(array[i] < min)
-				min = array[i];
-		}
-
 		System.out.println(min);
-		br.close();				
 	}
-
 }
