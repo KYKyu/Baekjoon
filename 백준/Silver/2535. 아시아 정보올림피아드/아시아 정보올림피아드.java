6d0e9a14d307b486b89c4147b696707b34b4d933
KYKyu <130036932+KYKyu@users.noreply.github.com>
2023-12-14 14:08:01 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 160 ms, Memory: 16368 KB -BaekjoonHub

---
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] arr = new int[N][4];
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < 3; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		Arrays.sort(arr, new Comparator<int[]>() {
+			public int compare(int[] o1, int[] o2) {
+				return o2[2]-o1[2];
+			}
+		});
+		
+		int dong = 2;
+		if(arr[0][0] == arr[1][0]) {
+			for(int i = 0; i < N; i++) {
+				if(arr[i][0] != arr[0][0]) {
+					dong = i;
+					break;
+				}
+			}
+		}
+		
+		sb.append(arr[0][0] + " " + arr[0][1] + "\n");
+		sb.append(arr[1][0] + " " + arr[1][1] + "\n");
+		sb.append(arr[dong][0] + " " + arr[dong][1]);
+		
+		System.out.println(sb);
+		br.close();
+		return;
+		
+		
+
+	}
+
+}
