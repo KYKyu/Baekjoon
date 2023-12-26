17e70d29f44e92cbc075771e2a7a883f4fd9c9d3
KYKyu <130036932+KYKyu@users.noreply.github.com>
2023-12-26 20:35:16 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 148 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String str = br.readLine();
+		int[] a = new int[26];
+
+		int len = str.length();
+		for(int i = 0; i < len; i++) {
+			a[str.charAt(i) - 97]++;
+		}
+		
+		for(int i = 0; i < 26; i++) {
+			sb.append(a[i] + " ");
+		}
+		System.out.println(sb);
+		return;
+
+	}
+
+}
