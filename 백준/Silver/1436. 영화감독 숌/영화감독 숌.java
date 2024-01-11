607ae79e345d2ae6364d3084e5dd5cf1aa5b805f
KYKyu <130036932+KYKyu@users.noreply.github.com>
2024-01-11 14:55:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 308 ms, Memory: 85092 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
+        int num = 666;
+
+        while(true){
+            if((num + "").contains("666")){
+                count++;
+            }
+
+            if(count == N){
+                System.out.println(num);
+                break;
+            }
+            num++;
+        }
+
+        br.close();
+    }
+}
