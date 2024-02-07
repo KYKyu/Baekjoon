f412cd73f9bdd308d01c377c3cee69130d3d9be0
KYKyu <130036932+KYKyu@users.noreply.github.com>
2024-02-07 18:52:56 +0900

    [Gold IV] Title: 오큰수, Time: 1060 ms, Memory: 144444 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -8,7 +8,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>(); // 더 큰수가 나올 때 까지 index를 담아둘 Stack
         int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
@@ -16,22 +16,10 @@ public class Main {
         }
 
         for (int i = 0; i < N; i++) {
-            if (stack.isEmpty()) {
-                stack.push(i);
-            } else {
-                while (true) {
-                    if (arr[stack.peek()] < arr[i]) {
-                        arr[stack.pop()] = arr[i];
-                        if (stack.isEmpty()) {
-                            stack.push(i);
-                            break;
-                        }
-                    } else {
-                        stack.push(i);
-                        break;
-                    }
-                }
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
             }
+            stack.push(i);
         }
 
         while (!stack.isEmpty()) {
