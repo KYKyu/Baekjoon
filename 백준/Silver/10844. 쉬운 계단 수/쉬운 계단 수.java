04296ffa00ba66c07c33d3e05af760ab7dd2cd42
KYKyu <130036932+KYKyu@users.noreply.github.com>
2024-03-19 16:46:55 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -4,32 +4,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N + 1][9 + 1];
         int[][] result = new int[N + 1][9 + 1];
 
         for (int i = 1; i < 10; i++) {
-            arr[1][i] = 1;
+            result[1][i] = 1;
         }
-
-        result = arr.clone();
 
         for (int i = 2; i < N + 1; i++) {
             for (int j = 0; j < 10; j++) {
                 if (j == 0) {
-                    arr[i][j] = result[i - 1][j + 1];
+                    result[i][j] = result[i - 1][j + 1];
                 } else if (j == 9) {
-                    arr[i][j] = result[i - 1][j - 1];
+                    result[i][j] = result[i - 1][j - 1];
                 } else {
-                    arr[i][j] = (result[i - 1][j + 1] + result[i - 1][j - 1]) % 1000000000;
+                    result[i][j] = (result[i - 1][j + 1] + result[i - 1][j - 1]) % 1000000000;
                 }
             }
-            result = arr.clone();
         }
 
         int sum = 0;
         for (int i = 0; i < 10; i++) {
             sum = (sum + result[N][i]) % 1000000000;
         }
+        
         System.out.println(sum % 1000000000);
         br.close();
     }
