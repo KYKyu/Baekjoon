2a5190f6dc346c30ea5b85c7e94d51ffb7df1193
KYKyu <130036932+KYKyu@users.noreply.github.com>
2024-03-25 12:16:55 +0900

    [Gold V] Title: 합분해, Time: 176 ms, Memory: 17200 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int mod = 1000000000;
+
+        int[][] dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i < K + 1; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            dp[i][1] = 1;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 2; j < K + 1; j++) {
+                for (int k = 0; k < i + 1; k++) {
+                    dp[i][j] = (dp[i][j] + dp[k][j - 1]) % mod;
+                }
+            }
+        }
+
+        System.out.println(dp[N][K] % mod);
+        br.close();
+    }
+}
